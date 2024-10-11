1166a3eb72943f17dac6bfd1d3ec84d4403cb0c0
Lawal Nurudeen <lawalnurudeenfocus@gmail.com>
2024-10-11 13:59:45 +0100

    Main | Add .prettierrc, create reusable styles and resolve main font

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,43 +2,43 @@
 import tailwindcssAnimate from 'tailwindcss-animate'
 
 export default {
-	content: [
-		'./index.html',
-		'./src/**/*.{js,ts,jsx,tsx,mdx}',
-		'./app/**/*.{js,ts,jsx,tsx,mdx}',
-		'./pages/**/*.{js,ts,jsx,tsx,mdx}',
-		'./components/**/*.{js,ts,jsx,tsx,mdx}',
-	],
-	theme: {
-		extend: {
-			container: {
-				center: true,
-				padding: {
-					DEFAULT: '1rem',
-					sm: '2rem',
-					lg: '4rem',
-					xl: '5rem',
-					'2xl': '6rem',
-				},
-				screens: {
-					sm: '640px',
-					md: '768px',
-					lg: '1024px',
-					xl: '1280px',
-					'2xl': '1536px',
-				},
-			},
-			fontFamily: {
-				bricolage: ['Bricolage Grotesque', 'sans-serif'],
-				playwrite: ['Playwrite IT Moderna', 'cursive'],
-				poppins: ['Poppins', 'sans-serif'],
-				roboto: ['Roboto Slab', 'serif'],
-				abel: ['Abel', 'sans-serif'],
-			},
-			colors: {
-				riseGreen: '#07969E',
-			},
-		},
-	},
-	plugins: [tailwindcssAnimate],
+  content: [
+    './index.html',
+    './src/**/*.{js,ts,jsx,tsx,mdx}',
+    './app/**/*.{js,ts,jsx,tsx,mdx}',
+    './pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './components/**/*.{js,ts,jsx,tsx,mdx}',
+  ],
+  theme: {
+    extend: {
+      container: {
+        center: true,
+        padding: {
+          DEFAULT: '1rem',
+          sm: '2rem',
+          lg: '4rem',
+          xl: '5rem',
+          '2xl': '6rem',
+        },
+        screens: {
+          sm: '640px',
+          md: '768px',
+          lg: '1024px',
+          xl: '1280px',
+          '2xl': '1536px',
+        },
+      },
+      fontFamily: {
+        schibsted: ['Schibsted Grotesk', 'sans-serif'],
+      },
+      colors: {
+        'rise-green': '#07969E',
+      },
+      backgroundImage: {
+        'custom-radial-gradient':
+          'radial-gradient(circle, #FFFFFF 0%, #FFFFFF 20%, #FFF4F0 40%, #F6F2FF 70%, #ECFEFE 100%)',
+      },
+    },
+  },
+  plugins: [tailwindcssAnimate],
 }
