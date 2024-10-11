93615c607b9e7457d30fb3da0654f0d0af91a1f6
Lawal Nurudeen <lawalnurudeenfocus@gmail.com>
2024-10-11 17:34:51 +0100

    Main | Add copyright marker

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -23,7 +23,7 @@ const productLinks = [
 
 const Footer = () => {
   return (
-    <footer className='bg-green-100 py-8 font-schibsted text-gray-800'>
+    <footer className='bg-green-100 pb-0 pt-8 font-schibsted text-gray-800'>
       <div className='section'>
         <div className='container-medium grid grid-cols-2 gap-8 md:px-20 lg:grid-cols-4'>
           {/* Logo Section */}
@@ -113,6 +113,12 @@ const Footer = () => {
             </ul>
           </div>
         </div>
+
+        <p className='mt-10 text-center text-sm text-gray-600'>
+          &copy; {new Date().getFullYear()} Created and completed by{' '}
+          <b>Nurudeen Lawal</b> <br />
+          Risevest Frontend Engineer Role
+        </p>
       </div>
     </footer>
   )
